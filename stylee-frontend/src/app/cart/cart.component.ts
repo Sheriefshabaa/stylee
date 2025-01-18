f39f6eb94f377679f3887702ce744f756a7bc68e
sheriefshabaan <sheriefshabaan1@hotmail.com>
2025-01-18 16:52:00 +0200

    O, ALLAH!

--- stylee-frontend/src/app/cart/cart.component.ts
+++ stylee-frontend/src/app/cart/cart.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, NgIterable} from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   standalone: false,
-  
+
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  cartItems?: any = [];
 
+  removeItem(item: any) {
+
+  }
+
+  updateQuantity(item: any, number: number) {
+
+  }
+
+  getTotalAmount() {
+    return 0;
+  }
+
+  checkout() {
+
+  }
 }
